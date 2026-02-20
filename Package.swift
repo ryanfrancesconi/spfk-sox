f8fec2c8004fa4ba69ed85639b0e790b56326214
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-19 19:25:56 -0800

    switch spfk package dependencies to version number

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", branch: "development"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", branch: "development"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
     ],
     targets: [
         .target(
